c01a184ba8eb20ff681cca18c18709009c51d6b0
e2ik <erik.jensen.lin@gmail.com>
2026-08-28 16:29:04 +1000

    playercombatcontroller: proper parry window code

--- Assets/Scripts/Player/PlayerCombatController.cs
+++ Assets/Scripts/Player/PlayerCombatController.cs
@@ -1,5 +1,3 @@
-using System;
-using Enemies;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,43 +8,37 @@ public enum ParryDirection
     Left,
     Right
 }
+
 public class PlayerCombatController : MonoBehaviour
 {
-    [Header("PlayerCombat")]
+    [Header("Parry Settings")]
+    // how long parry is active (to actually parry attacks
+    [SerializeField] private float parryActiveDuration = 0.2f;
+
+    // recovery window of when you can parry again ^ e.g. for 0.1 after active parry player is vulnerable
+    [SerializeField] private float parryRecoveryDuration = 0.3f;
+
+    // actual input buffer
+    [SerializeField] private float parryBufferTime = 0.15f;
+
+    private float parryActiveTimer;
+    private float parryRecoveryTimer;
+    private float parryBufferTimer;
+
+    private bool isParrying;
+    private bool isParryInRecovery;
+    private ParryDirection parryDir;
+
+    [Header("Other Combat References")]
     [SerializeField] private Transform attackOrigin;
-    private float attackRadius;
     public LayerMask enemyLayer;
 
-    private float attackDamage;
-    private float attackCrit; 
-    [SerializeField] private float critDamageMultiplier = 2f;
-    
-    private bool parryPressed;
-    private bool isParrying;
-    [SerializeField] private float parryDuration = 0.5f;
-    private float parryTimer;
-    private float parryCoolDown = 0.2f;
-    private ParryDirection parryDir;
-    
-    private bool attackPressed;
-    private bool isAttacking;
-    [SerializeField] private float attackDuration = 0.5f;
-    private float attackTimer;
-    private float attackCoolDown = 0.2f;
-    
-    private bool skillPressed;
-    private bool skillReleased;
-    private bool isSkilling;
-    private float skillTimer;
-    private float skillCoolDown = 0.2f;
-
     private float verticalInput;
-
     private PlayerController pController;
     private PlayerEquipmentManager equipmentManager;
 
-    public bool IsAttacking => isAttacking;
     public bool IsParrying => isParrying;
+    public bool IsParryInRecovery => isParryInRecovery;
 
     private void Awake()
     {
@@ -57,142 +49,103 @@ public class PlayerCombatController : MonoBehaviour
     private void Update()
     {
         if (!pController.MovementEnabled) return;
-        HandleParry();
-        HandleAttack();
-        HandleSkill();
+
+        UpdateTimers();
+        HandleParryLogic();
     }
 
-    private bool CanAttack()
+    private void UpdateTimers()
     {
-        return pController.MovementEnabled && !pController.IsWallSliding && !pController.IsChargingSkill;
-    }
-    
-    public bool CheckParry(ParryDirection parryDirection)
-    {
-        return isParrying && parryDir == parryDirection;
-    }
-    
-    private void HandleParry()
-    {
+        if (parryBufferTimer > 0f)
+            parryBufferTimer -= Time.deltaTime;
+
         if (isParrying)
-            parryTimer = parryCoolDown;
-        else
-            parryTimer -= Time.deltaTime;
-
-        if (parryPressed && parryTimer <= 0f && CanAttack())
         {
-            isParrying = true;
-            parryDir = pController.FacingDirection == 1 ? ParryDirection.Right : ParryDirection.Left;
-            if (verticalInput != 0)
+            parryActiveTimer -= Time.deltaTime;
+            if (parryActiveTimer <= 0f)
             {
-                if (verticalInput > 0.01f)
-                    parryDir = ParryDirection.Up;
-                else if (verticalInput < 0.01f && !pController.IsGrounded)
-                    parryDir = ParryDirection.Down;
+                isParrying = false;
+                isParryInRecovery = true;
+                parryRecoveryTimer = parryRecoveryDuration;
             }
+        }
 
-            parryPressed = false;
-            Invoke(nameof(StopParrying), parryDuration);
+        if (isParryInRecovery)
+        {
+            parryRecoveryTimer -= Time.deltaTime;
+            if (parryRecoveryTimer <= 0f)
+            {
+                isParryInRecovery = false;
+            }
         }
     }
-    private void StopParrying()
+
+    private void HandleParryLogic()
+    {
+        if (parryBufferTimer > 0f && CanParry())
+        {
+            ExecuteParry();
+        }
+    }
+
+    private bool CanParry()
+    {
+        return pController.MovementEnabled 
+            && !pController.IsWallSliding 
+            && !pController.IsChargingSkill 
+            && !isParrying 
+            && !isParryInRecovery;
+    }
+
+    private void ExecuteParry()
+    {
+        parryBufferTimer = 0f;
+
+        isParrying = true;
+        isParryInRecovery = false;
+        parryActiveTimer = parryActiveDuration;
+
+        parryDir = pController.FacingDirection == 1 ? ParryDirection.Right : ParryDirection.Left;
+
+        if (verticalInput > 0.01f)
+        {
+            parryDir = ParryDirection.Up;
+        }
+        else if (verticalInput < -0.01f && !pController.IsGrounded)
+        {
+            parryDir = ParryDirection.Down;
+        }
+
+        Debug.Log($"Parry Executed! Direction: {parryDir}");
+    }
+
+    public bool CheckParry(ParryDirection incomingDirection)
+    {
+        if (isParrying && parryDir == incomingDirection)
+        {
+            OnSuccessfulParry();
+            return true;
+        }
+        return false;
+    }
+
+    public void OnSuccessfulParry()
     {
         isParrying = false;
-    }
-    
-    private void HandleAttack()
-    {
-        if (isAttacking)
-            attackTimer = attackCoolDown;
-        else
-            attackTimer -= Time.deltaTime;
+        isParryInRecovery = false;
+        parryActiveTimer = 0f;
+        parryRecoveryTimer = 0f;
 
-        if (attackPressed && attackTimer <= 0f && CanAttack())
-        {
-            isAttacking = true;
-            
-            if (equipmentManager.EquippedWeapon == null)
-            {
-                Debug.LogWarning("[PlayerController] Cannot attack: No weapon equipped in PlayerEquipmentManager.");
-                return;
-            }
-
-            attackRadius = equipmentManager.EquippedWeapon.BaseWeaponRange;
-            attackDamage = equipmentManager.EquippedWeapon.BaseWeaponDamage;
-            attackCrit = equipmentManager.EquippedWeapon.BaseWeaponCrit;
-                
-            Collider2D[] enemiesInRange = Physics2D.OverlapCircleAll(attackOrigin.position, attackRadius, enemyLayer);
-            
-            float dmgAmount = attackDamage * (UnityEngine.Random.value <= attackCrit ? critDamageMultiplier : 1f);
-            enemiesInRange[0].GetComponent<EnemyHealth>().ApplyDamage((int)dmgAmount);
-            
-            Debug.Log("Primary Attack executed.");
-            
-            attackPressed = false;
-            Invoke(nameof(StopAttacking), attackDuration); //probably trigger this though animation events
-        }
-    }
-
-    public void StopAttacking()
-    {
-        isAttacking = false;
-    }
-    
-    private void HandleSkill()
-    {
-        if (isSkilling)
-            skillTimer = skillCoolDown;
-        else
-            skillTimer -= Time.deltaTime;
-
-        if (skillPressed && skillTimer <= 0f && CanAttack())
-        {
-            PrimaryGemBehaviourDefinition specialDef = equipmentManager.SpecialAttackDef;
-
-            if (specialDef == null)
-            {
-                Debug.LogWarning("[PlayerController] Cannot execute Special Attack: No Primary Gem equipped.");
-                return;
-            }
-
-            AttackContext context = equipmentManager.GetModifiedAttackContext();
-
-            specialDef.Execute(context);
-        }
-    }
-    
-    public void OnMove(InputValue value)
-    {
-        verticalInput = value.Get<Vector2>().y;
+        Debug.Log("PARRY SUCCESSFUL!");
     }
 
     public void OnParry()
     {
-        parryPressed = true;
-    }
-    
-    public void OnAttack()
-    {
-        attackPressed = true;
-    }
-    
-    public void OnSAttack(InputValue value)
-    {
-        if (value.isPressed)
-        {
-            skillPressed = true;
-            skillReleased = false;
-        }
-        else
-        {
-            skillPressed = false;
-            skillReleased = true;
-        }
+        parryBufferTimer = parryBufferTime;
     }
 
-    private void OnDrawGizmosSelected()
+    public void OnMove(InputValue value)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackOrigin.position, attackRadius);
+        verticalInput = value.Get<Vector2>().y;
     }
 }
