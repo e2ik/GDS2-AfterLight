340dfe5c19a9256f6f7b94631c56c19e60502b07
benhoweUTS <112325973+benhoweUTS@users.noreply.github.com>
2026-09-25 01:24:32 +1000

    edge detection logic update

--- Assets/Scripts/Player/PlayerEdgeDetection.cs
+++ Assets/Scripts/Player/PlayerEdgeDetection.cs
@@ -7,25 +7,28 @@ public class PlayerEdgeDetection : MonoBehaviour
     [SerializeField] private float radius;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private PlayerController pController;
-    private bool active;
+    private int groundContacts;
+    private bool isActive => groundContacts == 0;
 
     private void Update()
     {
-        if(active)
-            pController.onEdge = Physics2D.OverlapCircle(transform.position, radius, groundLayer);
+        Debug.Log(isActive);
+        pController.onEdge = isActive ? Physics2D.OverlapCircle(transform.position, radius, groundLayer) : false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-            active = false;
+        if (IsGroundLayer(other.gameObject.layer))
+            groundContacts++;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-            active = true;
+        if (IsGroundLayer(other.gameObject.layer))
+            groundContacts = Mathf.Max(0, groundContacts - 1);
     }
+
+    private bool IsGroundLayer(int layer) => (groundLayer.value & (1 << layer)) != 0;
 
     private void OnDrawGizmos()
     {
